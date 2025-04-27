bdd237eb642214a53611916ae28f457f649cd3eb
Khadijah Darragi <khadijahdarragi2003@gmail.com>
2025-04-27 21:19:11 +0100

    Update settings.py
    added the railway URL to CSRF trusted origins

--- cyclesync/settings.py
+++ cyclesync/settings.py
@@ -86,9 +86,9 @@ CORS_ALLOW_ALL_ORIGINS = True
 #     'X-CSRFToken',
 # ]
 
-# CSRF_TRUSTED_ORIGINS = [
-#     'http://127.0.0.1:8000',
-# ]
+CSRF_TRUSTED_ORIGINS = [
+    'https://cyclesync-backend-production.up.railway.app'
+]
 
 CSRF_COOKIE_SECURE = True
 CSRF_USE_SESSIONS = True
