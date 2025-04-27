46797d4f5398984ab0deb8f8eeb02229b8d8eaa6
Khadijah Darragi <khadijahdarragi2003@gmail.com>
2025-04-27 21:42:20 +0100

    Create custom user migration file

---
+++ api/migrations/0010_create_superuser.py
@@ -0,0 +1,23 @@
+# Generated manually to create initial superuser
+
+from django.db import migrations
+from api.models import CustomUser
+
+def create_superuser(apps, schema_editor):
+    """Create a superuser in the customer user model using CustomUserManager."""
+    user_manager = CustomUser.objects
+    user_manager.create_superuser(
+        email='khadijahdarragi2003@gmail.com',
+        password='edcsdf4321',
+    )
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('api', '0009_populate_initial_data'),  
+    ]
+
+    operations = [
+        migrations.RunPython(create_superuser),
+    ]
+ 
